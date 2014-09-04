841f995b9b0374d01b966c06366832282b13b3be
Justin Morris <desk@pixelbloom.com>
2014-09-04 20:47:13 +1000

    Precompile assets

--- lib/irradiated/tram_tracker/railtie.rb
+++ lib/irradiated/tram_tracker/railtie.rb
@@ -1,6 +1,9 @@
 module Irradiated
   module TramTracker
     class Railtie < ::Rails::Engine
+      initializer "Irradiated::TramTracker.assets.precompile" do |app|
+        app.config.assets.precompile += %w( TramTracker.css, TramTracker.js )
+      end
     end
   end
 end
